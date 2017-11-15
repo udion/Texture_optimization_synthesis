9a8e3612fe86f3ae070dbaecccf2f07d2f135949
udion <uddeshya.upa@gmail.com>
2017-11-15 22:10:33 +0530

    result saved, energy recorded

--- src/updateEX.m
+++ src/updateEX.m
@@ -1,4 +1,4 @@
-function [Xnext, X1_, X2_] = updateEX(Xcurr, Isample_spliced, cols_in_row, w, r)
+function [Xnext, X1_, X2_, en] = updateEX(Xcurr, Isample_spliced, cols_in_row, w, r)
 %UPDATEE It takes the current big picture and updates it in such a manner
 %that the energy after update is minimised, at pixels where multiple values
 %were possible it takes the average
@@ -11,7 +11,7 @@ function [Xnext, X1_, X2_] = updateEX(Xcurr, Isample_spliced, cols_in_row, w, r)
         for j=1:w/4:cmax
             Patch = Xcurr(i:i+w-1, j:j+w-1, :);
             %optimised
-            [simP, ~, ~, ~] = NNPatchSearchX(Patch, Isample_spliced, cols_in_row, r);
+            [simP, ~, ~, en] = NNPatchSearchX(Patch, Isample_spliced, cols_in_row, r);
             %
             Tens(i:i+w-1, j:j+w-1, :, 1) = Tens(i:i+w-1, j:j+w-1, :, 1)+simP;
             Tens(i:i+w-1, j:j+w-1, :, 2) = Tens(i:i+w-1, j:j+w-1, :, 2)+1;
