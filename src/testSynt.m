14bc858e7ac087a2ee2d14abf6bd2a277bfbdb2f
udion <uddeshya.upa@gmail.com>
2017-11-15 22:11:14 +0530

    run this to gen images

--- src/testSynt.m
+++ src/testSynt.m
@@ -1,4 +1,7 @@
 T = imread('../textures/minit4.jpg');
 %T = imresize(T,0.5);
 T = im2double(T);
-[o1, o2, o3] = synthesise(T,1,0.8);
+tic;
+clc;
+[ol11, ol12, ol13, ol21, ol22, ol23, ol31, ol32, ol33, toteng] = synthesise(T, 0.8, 64);
+toc;
